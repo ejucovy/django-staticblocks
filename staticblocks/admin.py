7aa1f36cabf83b8fcd5b59a31b810e92f007cd30
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-12-30 19:49:03 +0000

    better admin
    git-svn-id: https://my-svn.assembla.com/svn/arbez/django-staticblocks/trunk@1464 e0e46c49-be69-4f5a-ad62-21024a331aea

--- staticblocks/admin.py
+++ staticblocks/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from staticblocks.models import StaticBlock
 
-admin.site.register(StaticBlock)
+class Admin(admin.ModelAdmin):
+    list_display = ('name', 'content')
+
+admin.site.register(StaticBlock, Admin)
