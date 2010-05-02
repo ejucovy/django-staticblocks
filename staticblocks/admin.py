8a9f3c70654ca4bf627ad76ed44158877c0aa525
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-05-02 23:56:03 +0000

    scratch tinymce
    git-svn-id: https://my-svn.assembla.com/svn/arbez/django-staticblocks/trunk@1826 e0e46c49-be69-4f5a-ad62-21024a331aea

--- staticblocks/admin.py
+++ staticblocks/admin.py
@@ -6,23 +6,3 @@ class Admin(admin.ModelAdmin):
     list_display = ('name', 'content')
 
 admin.site.register(StaticBlock, Admin)
-
-
-# setup flatpages to use tiny_mce
-from django import forms
-from tinymce.widgets import TinyMCE
-from django.contrib.flatpages.models import FlatPage
-from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
-
-class FlatPageForm(forms.ModelForm):
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
-    
-    class Meta:
-        model = FlatPage
-
-class FlatPageAdmin(FlatPageAdminOld):
-    form = FlatPageForm
-
-# We have to unregister it, and then reregister
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
